d60fe4b6c891fc390250b34cad07ddf4cda79533
thiagodlopes <49046047+thiagodlopes@users.noreply.github.com>
2019-10-06 21:29:24 -0300

    Adiciona o modelo básico de lista encadeada
    Modelo base para referência. Necessita de aprimoramentos.

---
+++ AgendaDeContatos/AgendaDeContatos/Projeto/No.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaDeContatos {
+    public class No {
+        private int elemento;   //Define o tipo de dado que o nó irá comportar
+        private No proximo;     //Referencia a propria classe para apontar para o próximo elemento
+
+        public void setElemento(int elemento) {
+            this.elemento = elemento;
+        }
+        public int getElemento() {
+            return this.elemento;
+        }
+        public void setProximo(No proximo) {
+            this.proximo = proximo;
+        }
+        public No getProximo() {
+            return this.proximo;
+        }
+    }
+}
+
+
+
